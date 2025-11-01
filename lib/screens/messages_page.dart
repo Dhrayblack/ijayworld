71fc39c0ccb8b47078d9a6de7754d0457b05b533
Dhrayblack <Adetunjialex2015@gmail.com>
2025-11-01 19:05:26 +0000

    Initial commit

--- lib/screens/messages_page.dart
+++ lib/screens/messages_page.dart
@@ -1,16 +1,52 @@
 import 'package:flutter/material.dart';
 
-class MessagesPage extends StatelessWidget {
+class MessagesPage extends StatefulWidget {
   const MessagesPage({super.key});
+
+  @override
+  State<MessagesPage> createState() => _MessagesPageState();
+}
+
+class _MessagesPageState extends State<MessagesPage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(seconds: 2),
+      vsync: this,
+    )..repeat(reverse: true); // makes it bounce continuously
+
+    _animation = CurvedAnimation(
+      parent: _controller,
+      curve: Curves.bounceInOut,
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Messages')),
-      body: const Center(
-        child: Text(
-          'You have new messages!',
-          style: TextStyle(fontSize: 18),
+      body: Center(
+        child: ScaleTransition(
+          scale: _animation,
+          child: const Text(
+            'You have no messages!',
+            style: TextStyle(
+              fontSize: 22,
+              fontWeight: FontWeight.bold,
+              color: Colors.blueAccent,
+            ),
+          ),
         ),
       ),
     );
